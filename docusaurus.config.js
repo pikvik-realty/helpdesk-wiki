5a72c1456b18edeb1bcd004443736ad22f206612
pikvik-realty <exhome2024@gmail.com>
2024-12-10 14:04:23 +0900

    Fix editUrl settings

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,7 +44,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/pikvik-realty/helpdesk-wiki/',
+            'https://github.com/pikvik-realty/helpdesk-wiki/blob/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
