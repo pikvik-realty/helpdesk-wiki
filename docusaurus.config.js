87440356292143a1d81bfa2922f8e645226ab355
pikvik-realty <exhome2024@gmail.com>
2024-12-10 10:43:21 +0900

    Custom domain settings

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://pikvik-realty.github.io',
+  url: 'https://wiki.pikvik.net',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/helpdesk-wiki/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
